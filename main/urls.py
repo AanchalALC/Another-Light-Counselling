99ffafe59021c8303ec0fc4dc8e763d7414da24e
mahimajain0704 <mahimajain0704@gmail.com>
2022-04-17 12:35:01 +0530

    mahima commit 3
    changes for ppc leads

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,9 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('psychological-counselling-therapy', views.psychologicalCounsellingTherapy, name='psychological-counselling-therapy'),
-    path('psychological-counselling-therapy-thankyou', views.pctThankYou, name='psychological-counselling-therapy-thankyou'),
-    
+    path('psychological-counselling-therapy-thankyou', views.psychologicalCounsellingTherapy, name='psychological-counselling-therapy-thankyou'),
+    path('psychological-counselling-therapy-thankyou/', views.pctThankYou, name='psychological-counselling-therapy-thankyou'),
+
     path('about', views.about, name='about'),
     path('about/', views.about, name='about'),
 

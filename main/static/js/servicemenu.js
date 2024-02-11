0c324a5f21f4e89b7134005b6e461aaf73a9ecca
mahimajain0704 <mahima@forkmediagroup.com>
2024-02-11 21:49:10 +0530

    changes

--- main/static/js/servicemenu.js
+++ main/static/js/servicemenu.js
@@ -1,15 +1,27 @@
-function toggleDropdown(){
+function toggleDropdown() {
     // ONLY RUN IF WIDTH IS LESS THAN MOBILE SIZE
 
-    if (window.innerWidth < 790){
+    if (window.innerWidth < 790) {
         let dropdowndiv = document.getElementById('dropdowncontent')
         dropdowndiv.classList.toggle('visible')
     }
 }
 
-document.addEventListener('DOMContentLoaded', function(event){
-    
+function toggleDropdownDoIFeel() {
+    // ONLY RUN IF WIDTH IS LESS THAN MOBILE SIZE
+
+    if (window.innerWidth < 790) {
+        let dropdowndiv = document.getElementById('dropdowncontentdoifeel')
+        dropdowndiv.classList.toggle('visible')
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+
     let servicebutton = document.getElementById('servicebutton')
     servicebutton.addEventListener('click', toggleDropdown)
+
+    let doifeelbutton = document.getElementById('doifeelbutton')
+    doifeelbutton.addEventListener('click', toggleDropdownDoIFeel)
 
 })

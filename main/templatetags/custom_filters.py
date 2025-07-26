59881b6d32078e0ec085da62a731d7a238909751
Swathikrishna-0 <ssspriyamoru@gmail.com>
2025-07-26 23:24:43 +0530

    services updated

--- main/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-# core/templatetags/custom_filters.py
-from django import template
-
-register = template.Library()
-
-@register.filter
-def split_title(value):
-    """Return the part before the '|' character."""
-    return value.split('|')[0].strip()

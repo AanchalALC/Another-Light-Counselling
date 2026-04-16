6c7b81090d88b66ad98c60a7355987eeae55e58d
Swathikrishna-0 <ssspriyamoru@gmail.com>
2026-04-17 00:08:56 +0530

    Contact page revamped

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 from .models import Contact, PpcContact
 from django.forms import TextInput
+from django.core.validators import RegexValidator
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('name', 'number', 'instahandle',)
+        phone_regex = RegexValidator(
+        regex=r'^[\d\s\+\-\(\)]+$', 
+        message="Phone number must be valid. Only digits, spaces, and symbols like +, -, () are allowed."
+        )
+    
+        # Apply it to the field
+        number = forms.CharField(
+            validators=[phone_regex], 
+            max_length=20, 
+            required=True
+        )
         labels = {
             'name': '*What would you like us to call you?',
             'number': '*Your number?',
